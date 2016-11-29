276831cd08072590a1199052846957371dcc1e39
ALISON RAMER <ALISONRAMER@GMAIL.COM>
2016-11-29 14:56:38 -0800

    added lots of code

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 'use strict';
-alert('I AM ALIVE!');
+
+var question1 = prompt('Does Alison have any children?').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y' || question1 === 'yeah' || question1 === 'yea') {
+    alert('No, I am sorry you are wrong. Alison does not have children!');
+  } else if (question1 === 'no' || question1 === 'n') {
+    alert('Good job! you\'re right! Alison does not have children.');
+  } else {
+    alert('Try answering with yes or no');
+  }
