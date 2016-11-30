c844a86255053b5bb24426e51ac258a5a8b2113f
ALISON RAMER <ALISONRAMER@GMAIL.COM>
2016-11-30 10:39:19 -0800

    late night additions

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
 'use strict';
 
-var question1 = prompt('Does Alison have any children?').toLowerCase();
+var question1 = prompt('Do you know who Alice in Wonderland is?').toLowerCase();
 
 if (question1 === 'yes' || question1 === 'y' || question1 === 'yeah' || question1 === 'yea') {
-    alert('No, I am sorry you are wrong. Alison does not have children!');
-  } else if (question1 === 'no' || question1 === 'n') {
-    alert('Good job! you\'re right! Alison does not have children.');
-  } else {
-    alert('Try answering with yes or no');
-  }
+  alert('That\'s great! The time has come then...');
+  console.log('Alice in Wonderland, check');
+  document.write('We\'ve found someone who may be a match');
+}else if (question1 === 'no' || question1 === 'nah' || question1 === 'n') {
+  alert('No problem. I know someone who can help.');
+  console.log('This person has escaped British childrens literature');
+  document.write('We\'ve got a gem in the rough here');
+} else {
+  alert('Try answering with yes or no.');
+  console.log('We have some potentially indifference on our hands.');
+  document.write('We might need to go back to square one.');
+}
+
+var question2 = prompt('Have you ever believed an impossible thing?').toLowerCase();
+
+if (question2 === 'yes' || question2 === 'y' || question2 === 'yeah' || question2 === 'yea') {
+  alert('Than one more won\'t be any issue at all');
+}else if (question2 === 'no' || question2 === 'nah' || question2 === 'n') {
+  alert('Never? Well let\'s begin!');
+}else {
+  alert('Do you think answering is impossible?');
+}
+
+var question3 = prompt('Do you think it\s impossible to find a creative, responsible person?').toLowerCase();
+
+if (question3 === 'yes' || question3 === 'y' || question3 === 'yeah' || question3 === 'yea') {
+  alert('Than you\'ll never find them at all');
+}else if (question3 === 'no' || question3 === 'nah' || question3 === 'n') {
+  alert('Well than you are in luck! Prepare to go down the rabbit hole!');
+}else {
+  alert('Again no answer? Let\'s try the next question');
+}
+
+var question4 = prompt('Do you have a creative idea or endeavour?').toLowerCase();
+
+if (question4 === 'yes' || question4 === 'y' || question4 === 'yeah' || question4 === 'yea') {
+  alert('Than Alison is the professional for you!');
+}else if (question4 === 'no' || question4 === 'nah' || question4 === 'n') {
+  alert('If there is a will, there is a way. Alison can help you!');
+}else {
+  alert('Again no answer? Let\'s try the next question');
+}
+
+var question5 = prompt('Do you want to start today?').toLowerCase();
+
+if (question5 === 'yes' || question5 === 'y' || question5 === 'yeah' || question5 === 'yea') {
+  alert('I\'m sorry Alison isn\'t available. Please feel free to schedule another time by contacting her at alisonramer@gmail.com');
+  document.write(' Contact Alison at AlisonRamer@gmail.com');
+}else if (question5 === 'no' || question5 === 'nah' || question5 === 'n') {
+  alert('When ever you are ready, feel free to contact Alison at alisonramer@gmail.com');
+  document.write(' Contact Alison at AlisonRamer@gmail.com');
+}else {
+  alert('Again no answer? Contact Alison at alisonramer@gmail.com');
+  document.write(' Contact Alison at AlisonRamer@gmail.com');
+}
