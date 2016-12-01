946441afc3cd63e1aff06585be70520eb826cc20
ALISON RAMER <ALISONRAMER@GMAIL.COM>
2016-12-01 15:21:15 -0800

    added tallying and gold, silver, bronze function

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 'use strict';
 //
+
+var correctAnswers = 0;
+console.log(correctAnswers);
+
 //This is where the first five questions begin.
 function q1() {
   var question1 = prompt('Do you know who Alice in Wonderland is?').toLowerCase();
@@ -8,6 +12,7 @@ function q1() {
     alert('That\'s great! The time has come then...');
     console.log('Alice in Wonderland, check');
     document.write('We\'ve found someone who may be a match');
+    correctAnswers+=1;
   }else if (question1 === 'no' || question1 === 'nah' || question1 === 'n') {
     alert('No problem. I know someone who can help.');
     console.log('This person has escaped British childrens literature');
@@ -18,49 +23,56 @@ function q1() {
     document.write('We might need to go back to square one.');
   }
 }
-
+q1();
 function q2() {
   var question2 = prompt('Have you ever believed an impossible thing?').toLowerCase();
 
   if (question2 === 'yes' || question2 === 'y' || question2 === 'yeah' || question2 === 'yea') {
     alert('Than one more won\'t be any issue at all');
+    correctAnswers+=1;
   }else if (question2 === 'no' || question2 === 'nah' || question2 === 'n') {
     alert('Never? Well let\'s begin!');
   }else {
     alert('Do you think answering is impossible?');
   }
 }
+q2();
 
 function q3() {
   var question3 = prompt('Do you think it\s impossible to find a creative, responsible person?').toLowerCase();
 
   if (question3 === 'yes' || question3 === 'y' || question3 === 'yeah' || question3 === 'yea') {
     alert('Than you\'ll never find them at all');
+    correctAnswers+=1;
   }else if (question3 === 'no' || question3 === 'nah' || question3 === 'n') {
     alert('Well than you are in luck! Prepare to go down the rabbit hole!');
   }else {
     alert('Again no answer? Let\'s try the next question');
   }
 }
+q3();
 
 function q4 () {
   var question4 = prompt('Do you have a creative idea or endeavour?').toLowerCase();
 
   if (question4 === 'yes' || question4 === 'y' || question4 === 'yeah' || question4 === 'yea') {
     alert('Than Alison is the professional for you!');
+    correctAnswers+=1;
   }else if (question4 === 'no' || question4 === 'nah' || question4 === 'n') {
     alert('If there is a will, there is a way. Alison can help you!');
   }else {
     alert('Again no answer? Let\'s try the next question');
   }
 }
+q4();
 
 function q5 () {
-var question5 = prompt('Do you want to start today?').toLowerCase();
+  var question5 = prompt('Do you want to start today?').toLowerCase();
 
   if (question5 === 'yes' || question5 === 'y' || question5 === 'yeah' || question5 === 'yea') {
     alert('I\'m sorry Alison isn\'t available. Please feel free to schedule another time by contacting her at alisonramer@gmail.com');
     document.write(' Contact Alison at AlisonRamer@gmail.com');
+    correctAnswers+=1;
   } else if (question5 === 'no' || question5 === 'nah' || question5 === 'n') {
     alert('When ever you are ready, feel free to contact Alison at alisonramer@gmail.com');
     document.write(' Contact Alison at AlisonRamer@gmail.com');
@@ -69,7 +81,7 @@ var question5 = prompt('Do you want to start today?').toLowerCase();
     document.write(' Contact Alison at AlisonRamer@gmail.com');
   }
 }
-
+q5();
 //This is where the first five questions end
 
 //This is where question6 begins
@@ -80,6 +92,7 @@ while (question6 !== 6 && counter < 4) {
   question6 = parseInt(prompt('Can you guess how many days of the week Alison is working?'));
   if (question6 > 6) {
     alert ('Alison\'s Jewish. She observes Shabbat. That means one day off of work for her');
+    correctAnswers+=1;
   }
   else if (question6 < 6) {
     alert ('Work is play! Alison spends more time working than that.');
@@ -94,7 +107,6 @@ var placesIlived = ['SEATTLE','NEW YORK', 'TEL AVIV', 'JERUSALEM', 'RAMALLAH'];
 var breakingOutOfWhileLoop = false;
 var guesses = 0;
 
-console.log(answerToQuestion7);
 
 while (guesses < 6) {
   var answerToQuestion7 = prompt('Can you guess one of the places Alison has lived?').toUpperCase();
@@ -107,10 +119,24 @@ while (guesses < 6) {
   }//end of for if
   if (breakingOutOfWhileLoop) {
     alert ('Great job!');
+    correctAnswers +=1;
     break;
   }else {
     alert('Try again!');
   }
-  guesses++;
+  guesses+=1;
 } //end  of while loop
 //This is the end of question7
+console.log('Good job! You got ' + correctAnswers + ' of the answers correct');
+function awards () {
+  if (correctAnswers > 6) {
+    alert ('You know Alison! The Association for Knowledge about Alison is pleased to award you with our very, very special gold-plated medal.');
+  }
+  else if (correctAnswers >= 3 && correctAnswers <= 5) {
+    alert ('You\'re getting to know Alison. Good work! The Association for Knowledge About Alison is pleased to awarded you a silver-ish medal.');
+  }
+  else {
+    alert ('Thank you for your efforts. The Association for Knowledge About Alison is pleased to reward you with a bronze age metal.');
+  }
+}
+awards ();
